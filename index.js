19e86047553c25b1c88752aaad38083cb554ac02
David Solorzano <abdsp22@gmail.com>
2018-08-13 13:40:42 -0300

    fix post

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ class PuntoPago {
     createTransaction(url, trx_id, monto, medio_pago) {
 
         const funcion = 'transaccion/crear';
-        let monto_str = 2000;
         let data = { trx_id:trx_id, medio_pago:medio_pago, monto:monto }
         let header_array = this.TraerHeader(funcion, trx_id, medio_pago);
         axios({
@@ -23,22 +22,13 @@ class PuntoPago {
         .catch(function (error) {
             console.log(error);
         });
-
-        
-        
-
     }
-    //alo
+
     CrearTransaccionMP(trx_id, medio_pago, monto, url)
     {
         const funcion = 'transaccion/crear';
-        let monto_str = 2000;
-        let data = { trx_id:1231231357, medio_pago:"3", monto:20000 }
-        let header_array = this.TraerHeader(funcion, 1231231357, 20000);
-
-
-        console.log(header_array);
-        
+        let data = { trx_id:trx_id, medio_pago:medio_pago, monto:monto }
+        let header_array = this.TraerHeader(funcion, trx_id, monto);
 
         axios({
             method:'POST',
@@ -53,22 +43,11 @@ class PuntoPago {
             console.log(error);
         });
 
-
-        //return json_decode(this.ExecuteCommand(PUNTOPAGOS_URL.'/'.$funcion, $header_array, $data));
     }
 
     FirmarMensaje(str) {
-
-        let key = 123
-        //let signature = base64_encode(hash_hmac('sha1', $str, PUNTOPAGOS_SECRET, true));
-
-        //let signature = crypto.createHash('sha1', key).update(str).digest('base64')
-
         let signature = crypto.createHash('sha1').update(str).digest('base64')
-
-
         let object = ` PP PUNTOPAGOS_KEY: ${signature}`
-
 
         return object
          
@@ -97,8 +76,6 @@ class PuntoPago {
 
         let mensaje = `${funcion} \n ${token} \n ${trx_id} \n ${monto_str} \n ${date} \n`;
         
-
-
         let firma = this.FirmarMensaje(mensaje);
 
         var header_array = ['Accept: application/json',
