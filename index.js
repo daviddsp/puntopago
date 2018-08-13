ba92b11b8a08def499fdda581cbe76af4bb64590
David Solorzano <abdsp22@gmail.com>
2018-08-13 18:48:14 -0300

    add headers automatic

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ class PuntoPago {
             data: data
         })
         .then(function (response) {
-            console.log(response.data);
+
+            return response.data
+            //console.log(response.data);
         })
         .catch(function (error) {
-            console.log(error);
+            return error
+            //console.log(error);
         });
     }
 
@@ -26,25 +29,43 @@ class PuntoPago {
     {
         const funcion = 'transaccion/crear';
         let data = { trx_id:trx_id, medio_pago:medio_pago, monto:monto }
-        let header_array = this.TraerHeader(funcion, trx_id, monto);
+        //let header_array = this.TraerHeader(funcion, trx_id, monto);
+
+        var date = new Date();
+
+        let mensaje = `${process} \n ${trx_id} \n ${monto_str} \n ${date} \n`;
+
+        let firma = this.FirmarMensaje(mensaje, key, secret);
 
         axios({
             method:'POST',
             url:`${url}/${funcion}`,
-            headers: header_array,
+            headers: {
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                'Accept-Charset': 'application/x-www-form-urlencoded',
+                'Fecha': date,
+                'Autorizacion': firma,
+
+            },
+            json: true,
+            //headers: header_array,
             data: data
           })
         .then(function (response) {
-            console.log(response);
+            return response.data
+
         })
         .catch(function (error) {
-            console.log(error);
+            return error
+
         });
 
     }
 
     FirmarMensaje(str, key, secret) {
-        let signature = crypto.createHash('sha1').update(str,secret).digest('base64')
+        let signature = crypto.createHash('sha1', secret).update(str).digest('base64')
         let object = `PP ${key}: ${signature}`
 
         return object
@@ -59,12 +80,17 @@ class PuntoPago {
 
         let firma = this.FirmarMensaje(mensaje, key, secret);
 
-        var header_array = ['Accept: application/json',
-                              'Content-Type: application/json; charset=utf-8',
-                              'Accept-Charset: utf-8',
-                              `Fecha: ${date}`,
-                              `Autorizacion: ${firma}`
-                            ];
+        var header_array = [
+            'Accept: application/json',
+            'Access-Control-Allow-Origin: *',
+            'Content-Type: application/json',
+            'Accept-Charset: utf-8',
+            `Fecha: ${date}`,
+            `Autorizacion: ${firma}`
+        ];
+
+        console.log(header_array);
+        
 
         return header_array;
     }
@@ -76,12 +102,17 @@ class PuntoPago {
         
         let firma = this.FirmarMensaje(mensaje);
 
-        var header_array = ['Accept: application/json',
-                              'Content-Type: application/json; charset=utf-8',
-                              'Accept-Charset: utf-8',
-                              `Fecha: ${date}`,
-                              `Autorizacion: ${firma}`
-                            ];
+        var header_array = [
+            'Accept: application/json',
+            'Access-Control-Allow-Origin: *',
+            'Content-Type: application/json; charset=utf-8',
+            'Accept-Charset: utf-8',
+            `Fecha: ${date}`,
+            `Autorizacion: ${firma}`
+        ];
+
+        console.log(header_array);
+
 
         return header_array;
 
