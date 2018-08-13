76299659c7eed6275886388d6ca3b12e227a9c7e
David Solorzano <abdsp22@gmail.com>
2018-08-13 15:38:13 -0300

    agregando firma con key

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let crypto = require('crypto')
 const axios = require('axios');
 
-
-
 class PuntoPago {
 
-    createTransaction(url, trx_id, monto, medio_pago) {
+    createTransaction(url, trx_id, monto, medio_pago, key, secret) {
 
         const funcion = 'transaccion/crear';
         let data = { trx_id:trx_id, medio_pago:medio_pago, monto:monto }
-        let header_array = this.TraerHeader(funcion, trx_id, medio_pago);
+        let header_array = this.TraerHeader(funcion, trx_id, medio_pago, key, secret);
         axios({
             method:'POST',
             url:`${url}/${funcion}`,
@@ -24,7 +22,7 @@ class PuntoPago {
         });
     }
 
-    CrearTransaccionMP(trx_id, medio_pago, monto, url)
+    CrearTransaccionMP(url, trx_id, monto, medio_pago, key, secret)
     {
         const funcion = 'transaccion/crear';
         let data = { trx_id:trx_id, medio_pago:medio_pago, monto:monto }
@@ -45,21 +43,21 @@ class PuntoPago {
 
     }
 
-    FirmarMensaje(str) {
-        let signature = crypto.createHash('sha1').update(str).digest('base64')
-        let object = ` PP PUNTOPAGOS_KEY: ${signature}`
+    FirmarMensaje(str, key, secret) {
+        let signature = crypto.createHash('sha1').update(str,secret).digest('base64')
+        let object = `PP ${key}: ${signature}`
 
         return object
          
     }
 
-    TraerHeader(process, trx_id, monto_str) {
+    TraerHeader(process, trx_id, monto_str, key, secret) {
 
         var date = new Date();
 
         let mensaje = `${process} \n ${trx_id} \n ${monto_str} \n ${date} \n`;
 
-        let firma = this.FirmarMensaje(mensaje);
+        let firma = this.FirmarMensaje(mensaje, key, secret);
 
         var header_array = ['Accept: application/json',
                               'Content-Type: application/json; charset=utf-8',
